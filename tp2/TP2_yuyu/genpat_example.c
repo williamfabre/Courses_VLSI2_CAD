194e417315546e851c2c24247e68c41bb1e9b1c2
YS-DooMguy <55631175+YS-DooMguy@users.noreply.github.com>
2019-10-01 18:43:51 +0200

    tp2 finished need to do the report + bash script and makefile

--- tp2/TP2_yuyu/genpat_example.c
+++ tp2/TP2_yuyu/genpat_example.c
@@ -27,27 +27,27 @@
         DEF_GENPAT("example");
 
         /* interface */
+        DECLAR ("reset" ,":2", "B", IN, "", "" );
         DECLAR ("vdd"   ,":2", "B", IN, "", "" );
         DECLAR ("vss"   ,":2", "B", IN, "", "" );
         DECLAR ("ck"    ,":2", "B", IN, "", "" );
-        DECLAR ("reset" ,":2", "B", IN, "", "" );
         DECLAR ("i"     ,":2", "B", IN, "", "" );
         DECLAR ("o"     ,":2", "B", OUT,"", "" );
 
         LABEL ("cpt5");
         AFFECT ("0", "vdd", "0b1");
         AFFECT ("0", "vss", "0b0");
-        AFFECT ("0", "ck", "0b0");
+        AFFECT ("0", "ck", "0b1");
         AFFECT ("0", "reset", "0b1");
         AFFECT (inttostr(2*pas), "reset", "0b0");
+        AFFECT (inttostr(0), "o", "0B*");
         AFFECT ("0", "i", "0b0");
-        AFFECT ("0", "o", "0b0");
         
         for(nb=0; nb<N; nb++){
             for (c=0; c<nb; c++){
                 
-                AFFECT (inttostr( (cur_vect+0)*2*pas + pas), "ck", inttostr(1));
-                AFFECT (inttostr( (cur_vect+1)*2*pas +  0 ), "ck", inttostr(0));
+                AFFECT (inttostr( (cur_vect+0)*2*pas + pas), "ck", inttostr(0));
+                AFFECT (inttostr( (cur_vect+1)*2*pas +  0 ), "ck", inttostr(1));
 
                 if(c==0){
                     i=0;
@@ -61,7 +61,7 @@
                     else        o=0;
                 }
                 AFFECT (inttostr(cur_vect*2*pas +   pas), "i", inttostr(i) );
-                AFFECT (inttostr(cur_vect*2*pas + 0*pas), "o", inttostr(o) );
+                AFFECT (inttostr(cur_vect*2*pas + 2*pas), "o", inttostr(o) );
                 cur_vect++;
             }
         }
