afe5bb7e4e4a81f0d5ca9ce9db853896bed5c989
YS-DooMguy <55631175+YS-DooMguy@users.noreply.github.com>
2019-11-07 15:08:57 +0100

    Add files via upload

--- tp3/cordic_tb.vhd
+++ tp3/cordic_tb.vhd
@@ -12,13 +12,13 @@ ARCHITECTURE arch OF cordic_tb IS
  
 	SIGNAL reset : std_logic := '0';
 	SIGNAL ck : std_logic := '1';
-	SIGNAL A : std_logic_vector(8 DOWNTO 0) := "000000000";
+	SIGNAL A : std_logic_vector(9 DOWNTO 0) := "0000000000";
 	SIGNAL X : std_logic_vector(7 DOWNTO 0) := X"7F";
 	SIGNAL Y : std_logic_vector(7 DOWNTO 0) := X"00";
 	SIGNAL wr : std_logic := '0';
 	SIGNAL rd : std_logic := '0';
-	SIGNAL wok : std_logic;
-	SIGNAL rok : std_logic;
+	SIGNAL wok : std_logic := '0';
+	SIGNAL rok : std_logic :='0';
 	SIGNAL nX : std_logic_vector(7 DOWNTO 0);
 	SIGNAL nY : std_logic_vector(7 DOWNTO 0);
 	SIGNAL vdd : std_logic;
@@ -28,44 +28,30 @@ ARCHITECTURE arch OF cordic_tb IS
 	SIGNAL nY_ext : std_logic_vector(7 DOWNTO 0);
  
  
-	COMPONENT cordic
+	COMPONENT cordic_cor
 		PORT (
-			reset :  IN std_logic; 
-			ck :     IN std_logic; 
-			A :      IN std_logic_vector(8 DOWNTO 0); 
-			X :      IN std_logic_vector(7 DOWNTO 0); 
-			Y :      IN std_logic_vector(7 DOWNTO 0); 
-			wr :     IN std_logic; 
-			rd :     IN std_logic; 
-			wok :    OUT std_logic; 
-			rok :    OUT std_logic; 
-			nX :     OUT std_logic_vector(7 DOWNTO 0); 
-			nY :     OUT std_logic_vector(7 DOWNTO 0); 
-			vdd :    IN std_logic; 
-			vss :    IN std_logic 
-		);
+			ck          : IN  std_logic;                   
+			raz         : IN  std_logic;                   
+			                                               
+			wr_axy_p    : IN  std_logic;                   
+			a_p         : IN  std_logic_vector(9 DOWNTO 0);
+			x_p         : IN  std_logic_vector(7 DOWNTO 0);
+			y_p         : IN  std_logic_vector(7 DOWNTO 0);
+			wok_axy_p   : OUT std_logic;                   
+			                                               
+			rd_nxy_p    : IN  std_logic;                   
+			nx_p        : OUT std_logic_vector(7 DOWNTO 0);
+		    ny_p        : OUT std_logic_vector(7 DOWNTO 0);
+		    rok_nxy_p   : OUT std_logic                    
+		);       
 	END COMPONENT; 
 
 
  
 --Architecture BEGIN 
 BEGIN
-	cordic_instance : cordic
-	PORT MAP(
-		reset => reset, 
-		ck    => ck, 
-		A     => A, 
-		X     => X, 
-		Y     => Y, 
-		wr    => wr, 
-		rd    => rd, 
-		wok   => wok, 
-		rok   => rok, 
-		nX    => nX, 
-		nY    => nY, 
-		vdd   => vdd, 
-		vss   => vss
-	);
+	cordic_instance : cordic_cor
+	PORT MAP(ck, reset, wr, A, X, Y, wok, rd, nX, nY, rok);
 
     Stimulus_check: process
     procedure check(
@@ -74,9 +60,9 @@ BEGIN
         --reset, ck, A, X, Y, wr, rd, wok, rok, nX, nY, vdd, vss )  is
         variable i : integer := 0;
         begin
-        wait until rok = '1';
-            i := i + 1;
+        wait until (rok = '1' and not ck'stable);
             assert nX = nX_ext and nY = nY_ext report " error matching value(s) on output at image iteration " & integer'image(i) & ": nX value is " & integer'image(to_integer(signed(nX))) & " and must be " & integer'image(to_integer(signed(nX_ext))) & " ; and nY value is " & integer'image(to_integer(signed(nY))) & " and must be " & integer'image(to_integer(signed(nY_ext))) &";"severity error;--mettre des printf
+            i := i + 1;
         end procedure check;
         
         variable tt: integer;
@@ -87,12 +73,12 @@ BEGIN
             while not endfile(dat)loop
                 readline(dat,ll);
                 read(ll,tt);
-                wait on wok;
+                wait until wr =  '1' and not wr'stable;--wok;
                 nX_ext <= std_logic_vector(to_signed(tt,8));
                 readline(dat,ll);
                 read(ll,tt);
                 nY_ext <= std_logic_vector(to_signed(tt,8));
-                check(wok, rok, nX, nY, nX_ext, nY_ext);
+                --check(wok, rok, nX, nY, nX_ext, nY_ext);
             end loop;
             wait; --wait forever <3<3<3
   end process;
@@ -117,20 +103,26 @@ BEGIN
     process
     variable i : integer := 0;
     begin
+        wait for 30 ns;
         
         for i in 0 to 804 loop 
-        rd <= '0'; 
-        A <=  std_logic_vector( to_signed( i , 9 ) ) ;      
-        wr <=  '1' ;     
-        wait until wok = '1';
-        wr <= '0';
-        rd <=  '1';
-        wait until rok = '1';
-        end loop;     
+            --rd <= '0'; 
+            wait until (wok = '1' and ck'stable );-- not handshacking ie : on verifie wok avant le wr
+            A <=  std_logic_vector( to_signed( i , 10 ) ) ;      
+            wr <=  '1' ;     
+            wait for 2*CLK_PERIOD;
+            --wait for 100 ns;
+            wr <= '0';
+            rd <=  '1';
+            wait until ( rok = '1'and ck'stable );
+            wait for 2*CLK_PERIOD;
+            rd <= '0';
+        end loop; 
+            
         wait for CLK_PERIOD;
      
         ASSERT false REPORT "END OF test" SEVERITY note;
         -- Wait forever; this will finish the simulation.
-        wait;
+        --wait;
 	END PROCESS;
 END arch;
