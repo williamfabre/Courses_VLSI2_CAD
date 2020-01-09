d788ea8946d3261f7c60285d6b5e527dc80eba70
William Fabre <contact@williamfabre.fr>
2020-01-09 10:25:12 +0100

    corrections pour le tp5 + sujet (non fini)

--- tp5/hello.tcl
+++ tp5/hello.tcl
@@ -1,31 +1,38 @@
+# TP 5 MOCCA voir : https://www.tcl.tk/man/tcl8.5/tutorial/Tcl0.html
 # this is a comment
-set name [list "jack" "steve" "william" "franck" "edward"]
-#puts "Hello $name !"
+set name "prevetseski"
+puts "Hello $name !"
 
-#for { set i 0 } {$i<10} {incr i} {
-#puts "i => $name($i)"
-#}
 
-#foreach coder $name {
-#	puts $coder
-#}
+set name_list [list "jack" "steve" "william" "franck" "edward"]
+puts "Hello $name_list"
 
-#set third_coder [lindex $name 2]
-#puts "the nanana is : $third_coder"
 
-set days(d1) Monday
-set days(d2) Tuesday
-set days(d3) Wednesday
-set days(d4) Thursday
-set days(d5) Friday
-set days(d6) Saturday
-set days(d7) Sunday
-
-puts [array size days]
-
-foreach k [array names days] {
-	puts "key: $k val: $days($k)"
-	#puts $names
+# l'accolade doit etre sur la ligne du for.
+for { set i 0 } { $i < 10 } { incr i } {
+    puts " i => $i"
 }
 
+# creer un tableau
+set coders [list "kailun" "titi" "quentin" "sidiki"]
 
+# recupere une valeur a un index donne dans un tableau
+set third_coder [lindex $coders 2]
+
+# print une variable
+puts "the third coder is : $third_coder"
+
+# ajouter des valeurs dans un tableau et leur donner un index
+set foobarlol(word0) foo
+set foobarlol(word1) bar
+set foobarlol(word2) lol
+
+# recuperer la taille du tableau
+puts [array size foobarlol]
+
+# Boucle foreach
+# $k		    => index du tableau
+# $foobarlol($k)"   => acces dans le tableau parenthese pour acceder a la val
+ foreach k [array names foobarlol] {
+     puts "keys:  $k val: $foobarlol($k) \a"
+ }
