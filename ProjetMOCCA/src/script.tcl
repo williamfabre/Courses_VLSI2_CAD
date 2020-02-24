470366dba5ee14b1acdd9911344cd7c5818ef217
william <contact@williamfabre.fr>
2020-02-24 15:15:13 +0100

    little bit of cleaning

--- ProjetMOCCA/src/script.tcl
+++ ProjetMOCCA/src/script.tcl
@@ -1,52 +1,29 @@
-#start###################################################################
-puts "================="
-puts "Synthesis Started"
-date
-puts "================="
-#Include TCL utility scripts.
-#Set up variables.
+#RTL
 set rtl [list /users/enseig/fabre/work/MOCCA/ProjetMOCCA/src/mips_with_reset.vhd]
+#OPTIMISATION
 set_attribute endpoint_slack_opto true /
-
-##A CHANGE HERE IS REQUIRED##
+#NAME OF DESIGN
 set DESIGN MIPS_32_1P_MUL_DIV
-#set SYN_EFF <Required_synthesis_effort>
+#EFFORT FOR SYNTHESIS
 set SYN_EFF high 
-#set MAP_EFF <Required_mapping_effort>
 set MAP_EFF medium
-#set SYN_PATH <Required_working_directory>
 set SYN_PATH "."
-#set the PDK’s path as a variable ‘PDKDIR’
-#set PDKDIR $::env(PDKDIR)
-
-######################################################################
-#set the search path for the “.lib’ files provided with the PDK.
-#set_attribute lib_search_path $PDKDIR/gsclib045_all_v4.4/gsclib045/timing
-#select the needed .lib files.
-#set_attribute library { slow_vdd1v0_basicCells.lib}
+#LIBARRY
 set_attribute library /users/enseig/tuna/ue_vlsi2/techno/cmos_120/cmos_120nm_core_Worst.lib
 ######################################################################
-
-
-#This command is to read in your RTL code.
-##A CHANGE HERE IS REQUIRED##
-##Verilog##
+#READ VHDL
 read_hdl -vhdl $rtl
-#Elaboration validates the syntax.
+#ELABORATE AND VALIDATE SYNTAX
 elaborate $DESIGN
-
 #Reports the time and memory used in the elaboration.
 puts "Runtime & Memory after 'read_hdl'"
 timestat Elaboration
-#return problems with your RTL code.
+#CHECK UNRESOLVED IT'S THE WORST THAT COULD HAPPEN
 check_design -unresolved
-#Read in your clock difinition and timing constraints
-#read_sdc ./in/UP_COUNTER.sdc
+#CONSTRAINT FILE
 read_sdc /users/enseig/fabre/work/MOCCA/ProjetMOCCA/src/mips.sdc
 ######################################################################
-
-
-#Synthesizing to generic cell (not related to the used PDK)
+#Synthesizing to generic cell
 synthesize -to_generic -eff $SYN_EFF
 puts "Runtime & Memory after 'synthesize -to_generic'"
 timestat GENERIC
@@ -60,32 +37,18 @@ synthesize -to_mapped -eff $MAP_EFF -incr
 insert_tiehilo_cells
 puts "Runtime & Memory after incremental synthesis"
 timestat INCREMENTAL
-
-
-
 ######################################################################
 #write output files and generate reports
 report area > ./out/${DESIGN}_area.rpt
 report gates > ./out/${DESIGN}_gates.rpt
 report timing > ./out/${DESIGN}_timing.rpt
 report power > ./out/${DESIGN}_power.rpt
-#generate the verilog file with actual gates-> to be used in Encounter and ModelSim
-##Verilog##
+#Verilog
 write_hdl -mapped > ./out/${DESIGN}_map.v
-##SystemVerilog##
-#write_hdl -mapped > ”./out/${DESIGN}_map.sv
 #generate the constaraints file> to be used in Encounter
 write_sdc > ./out/${DESIGN}_map.sdc
-#generate the delays file> to be used in ModelSim
-write_sdf > ./out/${DESIGN}_map.sdf
-
-
-
 puts "Final Runtime & Memory."
 timestat FINAL
-#THE END
 puts "====================="
 puts "Synthesis Finished :)"
 puts "====================="
-#Exit RTL Compiler
-#quit
